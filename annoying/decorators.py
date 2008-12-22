3ff390911627daa7cf5c3e76876935f924d5d859
offline@home.example.com <offline@home.example.com>
2008-12-22 18:46:35 +0200

    init

---
+++ annoying/decorators.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def render_to(template=None):
+    """
+    Decorator for Django views that sends returned dict to render_to_response function
+    with given template from a string or variable and RequestContext as context instance.
+    If view doesn't return dict then decorator simply returns output.
+
+    Parameters:
+     - template: template name to use
+
+    Examples:
+    # 1
+
+    @render_to('template.html')
+    def foo(request):
+        bar = Bar.object.all()  
+        return {'bar': bar}
+
+    # equals to 
+    def foo(request):
+        bar = Bar.object.all()  
+        return render_to_response('template.html', 
+                                  {'bar': bar}, 
+                                  context_instance=RequestContext(request))
+
+    # 2
+
+    @render_to()
+    def foo(request, category):
+        template_name = '%s.html' % category
+        return {'bar': bar, 'TEMPLATE': template_name}
+    
+    #equals to
+    def foo(request, category):
+        template_name = '%s.html' % category
+        return render_to_response(template_name, 
+                                  {'bar': bar}, 
+                                  context_instance=RequestContext(request))
+
+    """
+    def renderer(function):
+        def wrapper(request, *args, **kwargs):
+            output = function(request, *args, **kwargs)
+            if not isinstance(output, dict):
+                return output
+            tmpl = output.pop('TEMPLATE', template)
+            return render_to_response(tmpl, output, context_instance=RequestContext(request))
+        return wrapper
+    return renderer
---
+++ annoying/functions.py
@@ -0,0 +1,19 @@
+from django.shortcuts import _get_queryset, get_object_or_404
+
+
+def get_object_or_None(klass, *args, **kwargs):
+    """
+    Uses get() to return an object or None if the object does not exist.
+
+    klass may be a Model, Manager, or QuerySet object. All other passed
+    arguments and keyword arguments are used in the get() query.
+
+    Note: Like with get(), an MultipleObjectsReturned will be raised if more than one
+    object is found.
+    """
+    queryset = _get_queryset(klass)
+    try:
+        return queryset.get(*args, **kwargs)
+    except queryset.model.DoesNotExist:
+        return None
+
