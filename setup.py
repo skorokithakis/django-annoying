3253462f5dd5ca1755e444b6a7340440b1622efc
Anderson <self.anderson@gmail.com>
2009-01-06 22:25:45 +0200

    setuptools setup file added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name = "django-annoying",
+    version = "0.1",
+    packages = find_packages(),
+    author = "Anderson",
+    author_email = "self.anderson@gmail.com",
+    description = "This is django application that try to eliminate annoying things in Django framework.",
+    license = "BSD",
+    keywords = "django",
+    url = "http://www.assembla.com/spaces/django-annoying",
+)
+
