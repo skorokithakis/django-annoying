926aa695219c1c784298c749bdc8d8a728508bd6
Anderson <self.anderson@gmail.com>
2009-01-31 14:34:51 +0200

    docstrings added to HttpResponseReload utility

--- annoying/utils.py
+++ annoying/utils.py
@@ -3,6 +3,21 @@ from django.utils.encoding import iri_to_uri
 
 
 class HttpResponseReload(HttpResponse):
+    """
+    Reload page and stay on the same page from where request was made.
+
+    example:
+
+    def simple_view(request):
+        if request.POST:
+            form = CommentForm(request.POST):
+            if form.is_valid():
+                form.save()
+                return HttpResponseReload(request)
+        else:
+            form = CommentForm()
+        return render_to_response('some_template.html', {'form': form})
+    """
     status_code = 302
 
     def __init__(self, request):
