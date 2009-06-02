0bf36a1726fc2e7c8a41f4091fb1ffffb58cd0c5
Anderson <self.anderson@gmail.com>
2009-06-02 14:30:57 +0300

    changes package version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "django-annoying",
-    version = "0.7.3",
+    version = "0.7.4",
     packages = find_packages(),
     author = "Anderson",
     author_email = "self.anderson@gmail.com",
