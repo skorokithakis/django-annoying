d5391c57257d1d56bf7df9f06289714168c7cbe8
Stavros Korokithakis <stavros@korokithakis.net>
2015-02-22 21:05:54 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="django-annoying",
-    version="0.8.0",
+    version="0.8.1",
     packages=find_packages(),
     author="Stavros Korokithakis",
     author_email="stavros@korokithakis.net",
