16b7af56af6ae600535cf10275caffbb833f834c
Steve Kossouho <artscoop93.info@gmail.com>
2015-08-29 14:43:23 +0200

    Python 3 changes

--- annoying/decorators.py
+++ annoying/decorators.py
@@ -237,7 +237,7 @@ def autostrip(cls):
 
     Author: nail.xx
     """
-    fields = [(key, value) for key, value in cls.base_fields.iteritems() if isinstance(value, forms.CharField)]
+    fields = [(key, value) for key, value in cls.base_fields.items() if isinstance(value, forms.CharField)]
     for field_name, field_object in fields:
         def get_clean_func(original_clean):
             return lambda value: original_clean(value and value.strip())
