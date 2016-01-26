90f881326a68cee8b38500e565d9f64d0af71dff
YPCrumble <ian@whatgoeswiththis.co>
2016-01-25 20:35:32 -0500

    Remove Python 2.6 classifier.

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
     ],
