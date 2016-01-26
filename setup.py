42c2bebf1bfef9d2e780af7e93a68470d4ecb5ea
YPCrumble <ian@whatgoeswiththis.co>
2016-01-25 20:35:50 -0500

    Bump version to 0.8.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="django-annoying",
-    version="0.8.5",
+    version="0.8.7",
     packages=find_packages(),
     author="Stavros Korokithakis",
     author_email="stavros@korokithakis.net",
